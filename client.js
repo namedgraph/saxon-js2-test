59008014d564aa27a3403686d630790b25aafb4b
Martynas <martynas@atomgraph.com>
2020-09-24 23:03:35 +0200

    Really fix CustomEvent detail

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ var customFetch = function(elem, url)
 {
 	fetch(url).
 	then(function(response) {
-		let event = new CustomEvent("customEvent", { "detail": { "response": response } });
+		let event = new CustomEvent("customEvent", { "detail": response });
 		// no need to add event listeners here, that is done by IXSL
 		document.dispatchEvent(event);
 	});
